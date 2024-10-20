55bfc0bc28a9a81825177cdf95178f8d57bfbc12
Sachin TS <sachints@Sachins-MacBook-Air.local>
2024-10-20 09:55:36 +0530

    model evaluation and prediction

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from TextSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from TextSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from TextSummarizer.pipeline.stage_05_modael_evaluation import ModelEvaluationTrainingPipeline
+
 
 
 from TextSummarizer.logging import logger
@@ -43,6 +45,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
